d10c7024297558de0680a606aceeb46b1c8d0edc
spencer <spencernhamilton@gmail.com>
2023-01-27 17:26:30 -0700

    done

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 abstract class Person {
     private String name;
     private String occupation;
@@ -33,16 +35,17 @@ class Teenager extends Person {
 
 
     public void answerQuestion() {
-
+        System.out.println("This party is lame.");
     }
 
     public void askQuestion() {
-
+        System.out.println("Why are there teachers at this party?");
     }
 }
 
 class Astronomer extends Person {
-    String[] responses = {};
+    String[] responses = {"I love space travel.", "Pluto is not a planet", "I can't wait for more information to be discovered about Mars."};
+    String[] questions = {"What's your favorite planet?", "Do you think Pluto is a planet?", "What do you think about human life on Mars?"};
 
     public Astronomer(String name, String occupation) {
         super(name, occupation);
@@ -55,18 +58,22 @@ class Astronomer extends Person {
     }
 
     public void answerQuestion() {
-        int index = (int) (Math.random() * responses.length);
-        System.out.println(responses[index]);
+        Random rand = new Random();
+        System.out.println(responses[rand.nextInt(responses.length)]);
     }
 
     public void askQuestion() {
-
+        Random rand = new Random();
+        System.out.println(questions[rand.nextInt(questions.length)]);
     }
 }
 
 class Teacher extends Person {
-    String[] responses = {};
     private String subject;
+    private String[] questions = {"Whats your favorite academic subject?", "Do you like to read?", "Do you like Shakespeare?"};
+    private String[] answers = {"I love math.", "I enjoy reading. Historical fiction is my favorite.", "I don't really like Shakespeare."};
+    private int answerIndex = 0;
+    private int questionIndex = 0;
 
     public Teacher(String name, String occupation, String subject) {
         super(name, occupation);
@@ -79,11 +86,15 @@ class Teacher extends Person {
         System.out.println("I teach " + subject);
     }
 
-    public void answerQuestion() {
-
+    public void askQuestion() {
+        if (questionIndex >= questions.length) questionIndex = 0;
+        System.out.println(questions[questionIndex]);
+        questionIndex++;
     }
 
-    public void askQuestion() {
-
+    public void answerQuestion() {
+        if (answerIndex >= answers.length) answerIndex = 0;
+        System.out.println(answers[answerIndex]);
+        answerIndex++;
     }
 }
